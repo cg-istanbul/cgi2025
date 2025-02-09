ac06e55487c42e81d68ec954579ebf6d6c2c9713
cg-istanbul <kayacan.selcuk@gmail.com>
2025-02-09 21:39:49 +0300

    Update maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -11,17 +11,10 @@ function initMap() {
     // }).addTo(map);
 
     
-    // L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-    //     attribution: '&copy; <a href="https://carto.com/">Carto</a> contributors',
-    // }).addTo(map);
-
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-        attribution: '&copy; <a href="https://carto.com/">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    // L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-    //    attribution: '&copy; <a href="https://carto.com/">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-        // subdomains: 'abcd',
-        // maxZoom: 20
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://carto.com/">Carto</a> contributors',
     }).addTo(map);
+
 
     
     // Add a marker for Bahçeşehir University
