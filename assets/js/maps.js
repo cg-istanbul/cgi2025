cf6ff4541b605535fff8f5dfa95689ff9ae8778a
cg-istanbul <kayacan.selcuk@gmail.com>
2025-02-09 21:36:17 +0300

    Update maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -15,8 +15,10 @@ function initMap() {
     //     attribution: '&copy; <a href="https://carto.com/">Carto</a> contributors',
     // }).addTo(map);
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://carto.com/">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    // L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+    //    attribution: '&copy; <a href="https://carto.com/">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         // subdomains: 'abcd',
         // maxZoom: 20
     }).addTo(map);
